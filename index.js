e892da28d68517d0ce61ae7d4f33e7ed3e0b56d9
resikeshroot <resikesmr123@gmail.com>
2025-11-10 11:19:20 +0530

    Add GitHub Actions CI

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("CI/CD Pipeline Demo Running!"));
+app.get("/", (req, res) => res.send("CI/CD Pipeline Demo Running! its now running in locally"));
 app.listen(3000, () => console.log("Server started on port 3000"));
